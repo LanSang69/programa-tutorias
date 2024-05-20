3e3561f43711cb21755fa4d77f4a300b44486c65
LanSang69 <alanisg029@gmail.com>
2024-05-20 14:24:48 -0600

    Agregando cosas final

---
+++ assets/js/checarForm.js
@@ -0,0 +1,88 @@
+//Inputs
+var nameElement = document.getElementById('name');
+var lastNameElement = document.getElementById('lastName');
+var sLastNameElement = document.getElementById('sLastName');
+var emailElement = document.getElementById('email');
+var boletaElement = document.getElementById('boleta');
+var telefonoElement = document.getElementById('telefono');
+var passwordElement = document.getElementById('password');
+var submitButton = document.querySelector('input[type="submit"]');
+
+//divs de error
+const nInvalidElement = document.querySelector('.n-invalid');
+const lnInvalidElement = document.querySelector('.ln-invalid');
+const slnInvalidElement = document.querySelector('.sln-invalid');
+const emailInvalidElement = document.querySelector('.email-invalid');
+const boletaInvalidElement = document.querySelector('.boleta-invalid');
+const telInvalidElement = document.querySelector('.tel-invalid');
+const passwordInvalidElement = document.querySelector('.password-invalid');
+
+function isEmpty(value){
+    return value.trim() == '';
+}
+
+function validarFormulario() {
+    const nameValid = validarNombre(nameElement.value);
+    const lastNameValid = validarNombre(lastNameElement.value);
+    const sLastNameValid = validarNombre(sLastNameElement.value);
+    const emailValid = validarCorreo(emailElement.value);
+    const telefonoValid = validarTelefono(telefonoElement.value);
+    const boletaValid = validarBoleta(boletaElement.value);
+    const passwordValid = validarContra(passwordElement.value);
+
+    const allFieldsValid = nameValid && lastNameValid && sLastNameValid && emailValid && telefonoValid && boletaValid && passwordValid;
+
+    submitButton.disabled = !allFieldsValid;
+}
+
+function toggleInvalidClass(element, isValid, value) {
+    if (isValid || isEmpty(value)) {
+        element.classList.remove('show');
+        element.classList.add('hide');
+    } else {
+        element.classList.add('show');
+        element.classList.remove('hide');
+    }
+}
+
+nameElement.addEventListener('input', function() {
+    var value = nameElement.value;
+    toggleInvalidClass(nInvalidElement, validarNombre(value), value);
+    validarFormulario();
+});
+
+lastNameElement.addEventListener('input', function() {
+    var value = lastNameElement.value;
+    toggleInvalidClass(lnInvalidElement, validarNombre(value), value);
+    validarFormulario();
+});
+
+sLastNameElement.addEventListener('input', function() {
+    var value = sLastNameElement.value;
+    toggleInvalidClass(slnInvalidElement, validarNombre(value), value);
+    validarFormulario();
+});
+
+emailElement.addEventListener('input', function() {
+    var value = emailElement.value;
+    toggleInvalidClass(emailInvalidElement, validarCorreo(value), value);
+    validarFormulario();
+});
+
+telefonoElement.addEventListener('input', function(){
+    var value = telefonoElement.value;
+    toggleInvalidClass(telInvalidElement, validarTelefono(value), value);
+    validarFormulario();
+});
+
+boletaElement.addEventListener('input', function(){
+    var value = boletaElement.value;
+    toggleInvalidClass(boletaInvalidElement, validarBoleta(value), value);
+    validarFormulario();
+});
+
+passwordElement.addEventListener('input', function(){
+    var value = passwordElement.value;
+    toggleInvalidClass(passwordInvalidElement, validarContra(value), value);
+    validarFormulario();
+});
---
+++ assets/js/validacion.js
@@ -0,0 +1,21 @@
+let regexCorreo = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+let regexNumero = /\d{10}/;
+let regexNombres = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ]+(\s[a-zA-ZáéíóúÁÉÍÓÚñÑ]+)*$/;
+let regexBoleta = /^20\d{8}/;
+let regexContraseña = /^(?=.*[a-zA-ZáéíóúÁÉÍÓÚñÑ])(?=.*\d)(?=.*[.,@#$%&*_\-+=!])[a-zA-ZáéíóúÁÉÍÓÚñÑ\d.,@#$%&*_\-+=!]+$/;
+
+function validarNombre(nombre) {
+    return regexNombres.test(nombre);
+}
+function validarCorreo(correo) {
+    return regexCorreo.test(correo);
+}
+function validarBoleta(boleta) {
+    return regexBoleta.test(boleta);
+}
+function validarContra(contra){
+    return regexContraseña.test(contra);
+}
+function validarTelefono(telefono) {
+    return regexNumero.test(telefono);
+}
